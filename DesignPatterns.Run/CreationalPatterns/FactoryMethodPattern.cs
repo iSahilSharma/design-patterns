2f2d74dab94792dd7adbcd3a9367913a27be7015
Sahil Sharma <isahilsharma@outlook.com>
2023-09-17 23:22:27 +0530

    Factory Method example added

--- DesignPatterns.Run/CreationalPatterns/FactoryMethodPattern.cs
+++ DesignPatterns.Run/CreationalPatterns/FactoryMethodPattern.cs
@@ -1,6 +1,159 @@
 using System;
 
+// 
 namespace DesignPatterns.CreationalPatterns.FactoryMethod
 {
+    #region Factory Method (Example 1)
+    // 1.[Product] Define an interface for the products (file readers).
+    public interface IFileReader
+    {
+        void Read(string filePath);
+    }
 
+    // 2.[Concrete Products] Create concrete product classes (PDF and Text file readers).
+    public class PdfFileReader : IFileReader
+    {
+        public void Read(string filePath)
+        {
+            Console.WriteLine($"Reading PDF file: {filePath}");
+            // Code to read PDF file
+        }
+    }
+
+    public class TextFileReader : IFileReader
+    {
+        public void Read(string filePath)
+        {
+            Console.WriteLine($"Reading Word document: {filePath}");
+            // Code to read Word document
+        }
+    }
+
+    // 3.[Creator] Create an abstract creator class (FileProcessor).
+    public abstract class FileProcessor
+    {
+        // 4. Declare a factory method for creating product objects.
+        public abstract IFileReader CreateFileReader();
+
+        // 5. Use the factory method to create products (file readers).
+        public void ProcessFile(string filePath)
+        {
+            // 5a. Use the factory method to create a product (file reader).
+            IFileReader fileReader = CreateFileReader();
+
+            // 5b. Use the created product (file reader) to process the file.
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                fileReader.Read(filePath);
+                // Additional processing logic
+            }
+            else
+            {
+                Console.WriteLine("Unsupported file format.");
+            }
+        }
+    }
+
+    // 6.[Concrete Creators] Create concrete creator classes (PdfFileProcessor and TextFileProcessor).
+    public class PdfFileProcessor : FileProcessor
+    {
+        // 7. Implement the factory method to return a specific product (PDF file reader).
+        public override IFileReader CreateFileReader()
+        {
+            // 7a. Create and return a specific product (PDF file reader).
+            return new PdfFileReader();
+        }
+    }
+
+    public class TextFileProcessor : FileProcessor
+    {
+        // 8. Implement the factory method to return a specific product (Text file reader).
+        public override IFileReader CreateFileReader()
+        {
+            // 8a. Create and return a specific product (Text file reader).
+            return new TextFileReader();
+        }
+    }
+
+    public class TestFileProcessor
+    {
+        public void EntryPoint()
+        {
+            FileProcessor pdfProcessor = new PdfFileProcessor();
+            FileProcessor wordProcessor = new TextFileProcessor();
+
+            pdfProcessor.ProcessFile("sample.pdf");
+            wordProcessor.ProcessFile("document.docx");
+        }
+    }
+    #endregion
+
+    #region Factory Method (Example 2)
+    public enum PaymentMode
+    {
+        DebitCard,
+        CreditCard
+    }
+
+    // Product: Payment Processor
+    public interface IPaymentProcessor
+    {
+        void ProcessPayment(decimal amount);
+    }
+
+    // Concrete Product: Debit Card Processor
+    public class DebitCardProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine("Processing Debit Card payment.");
+            // Actual Debit Card processing logic
+        }
+    }
+
+    // Concrete Product: Credit Card Processor
+    public class CreditCardProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine("Processing Credit Card payment.");
+            // Actual Credit Card processing logic
+        }
+    }
+
+    // Creator: Payment Processor Factory
+    public class PaymentProcessorFactory
+    {
+        public IPaymentProcessor CreatePaymentProcessor(PaymentMode paymentMode)
+        {
+            switch (paymentMode)
+            {
+                case PaymentMode.DebitCard:
+                    return new DebitCardProcessor();
+
+                case PaymentMode.CreditCard:
+                    return new CreditCardProcessor();
+
+                default:
+                    throw new NotImplementedException("Payment Processor not defined.");
+            }
+        }
+    }
+
+    // Test Class to Demonstrate Factory Usage
+    public class TestPaymentProcessorFactory
+    {
+        public void EntryPoint()
+        {
+            PaymentProcessorFactory factory = new PaymentProcessorFactory();
+
+            // User selects a payment method (e.g., from user input)
+            // Create the appropriate payment processor using the factory
+            IPaymentProcessor paymentProcessor = factory.CreatePaymentProcessor(PaymentMode.DebitCard);
+
+            // Process the payment
+            paymentProcessor.ProcessPayment(100.0m);
+        }
+    }
+    #endregion
 }
