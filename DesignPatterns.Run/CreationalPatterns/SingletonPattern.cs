cda5b0cd054648597027dea0f8723bed58e263c7
Sahil Sharma <isahilsharma@outlook.com>
2023-09-29 18:16:51 +0530

    Code formatting

--- DesignPatterns.Run/CreationalPatterns/SingletonPattern.cs
+++ DesignPatterns.Run/CreationalPatterns/SingletonPattern.cs
@@ -2,53 +2,62 @@ using System;
 
 namespace DesignPatterns.CreationalPatterns.SingletonPattern
 {
+       #region Approach One (without Lazy operator)
+       // Singleton Logger class implementing the ILogger interface
+       public class ConfigurationManager
+       {
+              // Private static instance variable
+              private static ConfigurationManager _instance;
+
+              // Private object for locking during instance creation
+              private static readonly object lockObject = new object();
+
+              // Private constructor to prevent external instantiation
+              private IConfiguration _configuration;
+
+              private ConfigurationManager()
+              {
+                     // Load application configuration settings using .NET Core's IConfiguration
+                     var builder = new ConfigurationBuilder()
+                         .SetBasePath(AppContext.BaseDirectory)
+                         .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+                     _configuration = builder.Build();
+              }
+
+              // Public static method to access the Singleton instance
+              public static ConfigurationManager Instance
+              {
+                     get
+                     {
+                            if (_instance == null)
+                            {
+                                   lock (LockObject)
+                                   {
+                                          if (_instance == null)
+                                          {
+                                                 _instance = new ConfigurationManager();
+                                          }
+                                   }
+                            }
+                            return _instance;
+                     }
+              }
+
+              public string GetSetting(string key)
+              {
+                     return _configuration[key];
+              }
+       }
+       #endregion
+
+       #region Approach Two (with Lazy operator)
        // Define the ILogger interface
        public interface ILogger
        {
               void Log(string message);
        }
 
-       #region Approach One (without Lazy operator)
-       // Singleton Logger class implementing the ILogger interface
-       public class Logger : ILogger
-       {
-              // Private static instance variable
-              private static Logger _instance = null;
-
-              // Private object for locking during instance creation
-              private static readonly object lockObject = new object();
-
-              // Private constructor to prevent external instantiation
-              private Logger() { }
-
-              // Public static method to access the Singleton instance
-              public static Logger GetInstance()
-              {
-                     if (_instance == null)
-                     {
-                            lock (lockObject)
-                            {
-                                   // Double-check locking to ensure thread safety
-                                   if (_instance == null)
-                                   {
-                                          _instance = new Logger();
-                                   }
-                            }
-                     }
-
-                     return _instance;
-              }
-
-              // Log method to log a message
-              public void Log(string message)
-              {
-                     // Replace this line with actual logging code
-                     Console.WriteLine($"Logged message: {message}");
-              }
-       }
-       #endregion
-
-       #region Approach Two (with Lazy operator)
        // Singleton Logger class implementing the ILogger interface
        public class LazyLogger : ILogger
        {
