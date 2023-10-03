cf7c3d68429fca582ac05bc9c24fb00ea70078be
Sahil Sharma <isahilsharma@outlook.com>
2023-10-03 20:06:10 +0530

    Adapter pattern

--- DesignPatterns.Run/StructuralPatterns/AdapterPattern.cs
+++ DesignPatterns.Run/StructuralPatterns/AdapterPattern.cs
@@ -2,8 +2,78 @@ using System;
 
 namespace DesignPatterns.StructuralPatterns.Adapter
 {
-    public class Adapter
+    // Step 1: Define the standard payment processing interface
+    public interface IPaymentProcessor
     {
-
+        void ProcessPayment(decimal amount);
     }
+
+    // Step 2: Create concrete payment processor for the system
+    public class PaymentProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing payment of {amount:C} using the standard payment processor.");
+        }
+    }
+
+    // Step 3: Define the interface of an incompatible payment gateway
+    public interface IThirdPartyPaymentGateway
+    {
+        void Pay(decimal amount);
+    }
+
+    // Step 4: Create an adapter to make the third-party gateway compatible
+    public class ThirdPartyPaymentGatewayAdapter : IPaymentProcessor
+    {
+        private readonly IThirdPartyPaymentGateway gateway;
+
+        public ThirdPartyPaymentGatewayAdapter(IThirdPartyPaymentGateway gateway)
+        {
+            this.gateway = gateway;
+        }
+
+        public void ProcessPayment(decimal amount)
+        {
+            // Adapt the ProcessPayment method to call Pay on the third-party gateway
+            gateway.Pay(amount);
+        }
+    }
+
+    // Step 5: Implement the third-party payment gateways
+    public class PayPalGateway : IThirdPartyPaymentGateway
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid {amount:C} via PayPal.");
+        }
+    }
+
+    public class StripeGateway : IThirdPartyPaymentGateway
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid {amount:C} via Stripe.");
+        }
+    }
+
+    // Step 6: Client code that uses the payment processing system
+    class Program
+    {
+        static void Main()
+        {
+            // Create a standard payment processor
+            IPaymentProcessor standardProcessor = new PaymentProcessor();
+
+            // Create adapters for third-party payment gateways
+            IPaymentProcessor paypalAdapter = new ThirdPartyPaymentGatewayAdapter(new PayPalGateway());
+            IPaymentProcessor stripeAdapter = new ThirdPartyPaymentGatewayAdapter(new StripeGateway());
+
+            // Process payments using different processors
+            standardProcessor.ProcessPayment(100.0m);
+            paypalAdapter.ProcessPayment(50.0m);
+            stripeAdapter.ProcessPayment(75.0m);
+        }
+    }
+
 }
